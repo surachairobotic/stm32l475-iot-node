6ea5a6adbd382263135c046c5c828769bf78c6a0
Surachai Rodsai <surachairobotic@gmail.com>
2020-02-24 21:51:01 +0700

    setting global MBED_OS_DIR and prepare sub project

---
+++ BUILD/DISCO_L475VG_IOT01A/GCC_ARM/mbed-os/drivers/source/TableCRC.d
@@ -0,0 +1,4 @@
+BUILD/DISCO_L475VG_IOT01A/GCC_ARM/mbed-os/drivers/source/TableCRC.o: \
+ mbed-os/drivers/source/TableCRC.cpp \
+ BUILD/DISCO_L475VG_IOT01A/GCC_ARM/mbed_config.h \
+ mbed-os/drivers/internal/TableCRC.h
