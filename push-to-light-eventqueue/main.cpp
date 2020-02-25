1981858ed089e12c814abc03e059762ff467ca9b
Surachai Rodsai <surachairobotic@gmail.com>
2020-02-25 15:44:28 +0700

    update cmd in memo.txt

--- push-to-light-eventqueue/main.cpp
+++ push-to-light-eventqueue/main.cpp
@@ -1,4 +1,5 @@
 #include "mbed.h"
+#include "rtos.h"
 
 DigitalOut led1(LED1);
 InterruptIn sw(PC_13);
@@ -6,13 +7,11 @@ EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
 
 void rise_handler(void) {
-    printf("rise_handler in context %p\r\n", Thread::gettid());
     // Toggle LED
     led1 = !led1;
 }
 
 void fall_handler(void) {
-    printf("fall_handler in context %p\r\n", Thread::gettid());
     // Toggle LED
     led1 = !led1;
 }
@@ -20,7 +19,6 @@ void fall_handler(void) {
 int main() {
     // Start the event queue
     t.start(callback(&queue, &EventQueue::dispatch_forever));
-    printf("Starting in context %p\r\n", Thread::gettid());
     // The 'rise' handler will execute in IRQ context
     sw.rise(rise_handler);
     // The 'fall' handler will execute in the context of thread 't'
