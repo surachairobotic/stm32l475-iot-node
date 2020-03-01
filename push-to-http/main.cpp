1a4afdaeb75d2a23f4df20e7933e2b3f22102072
Surachai Rodsai <surachairobotic@gmail.com>
2020-03-01 15:38:20 +0700

    push-to-http with mbed-os-5.14.0

--- push-to-http/main.cpp
+++ push-to-http/main.cpp
@@ -8,27 +8,7 @@ InterruptIn button(USER_BUTTON);
 Thread t;
 EventQueue queue(5 * EVENTS_EVENT_SIZE);
 Serial pc(USBTX, USBRX);
-WiFiInterface *wifi;
 
-
-const char *sec2str(nsapi_security_t sec)
-{
-    switch (sec) {
-        case NSAPI_SECURITY_NONE:
-            return "None";
-        case NSAPI_SECURITY_WEP:
-            return "WEP";
-        case NSAPI_SECURITY_WPA:
-            return "WPA";
-        case NSAPI_SECURITY_WPA2:
-            return "WPA2";
-        case NSAPI_SECURITY_WPA_WPA2:
-            return "WPA/WPA2";
-        case NSAPI_SECURITY_UNKNOWN:
-        default:
-            return "Unknown";
-    }
-}
 
 void dump_response(HttpResponse* res) {
     printf("Status: %d - %s\n", res->get_status_code(), res->get_status_message().c_str());
@@ -38,37 +18,6 @@ void dump_response(HttpResponse* res) {
         printf("\t%s: %s\n", res->get_headers_fields()[ix]->c_str(), res->get_headers_values()[ix]->c_str());
     }
     printf("\nBody (%d bytes):\n\n%s\n", res->get_body_length(), res->get_body_as_string().c_str());
-}
-
-int scan_wifi() {
-    WiFiAccessPoint *ap;
-
-    printf("Scan:\n");
-    int count = wifi->scan(NULL,0);
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
-    }
-
-    /* Limit number of network arbitrary to 15 */
-    count = count < 15 ? count : 15;
-    ap = new WiFiAccessPoint[count];
-    count = wifi->scan(ap, count);
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
-    }
-
-    for (int i = 0; i < count; i++) {
-        printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\n", ap[i].get_ssid(),
-               sec2str(ap[i].get_security()), ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-               ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5], ap[i].get_rssi(), ap[i].get_channel());
-    }
-    printf("%d networks available.\n", count);
-
-    delete[] ap;   
-
-    return count; 
 }
 
 int http() {
@@ -121,61 +70,13 @@ int http() {
 }
 
 void pressed_handler() {
-/*    int count;
-
-    count = scan_wifi();
-    if (count == 0) {
-        pc.printf("No WIFI APs found - can't continue further.\n");
-        return;
-    }
-
-    pc.printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-        pc.printf("\nConnection error: %d\n", ret);
-        return;
-    }
-
-    pc.printf("Success\n\n");
-    pc.printf("MAC: %s\n", wifi->get_mac_address());
-    SocketAddress a;
-    wifi->get_ip_address(&a);
-    pc.printf("IP: %s\n", a.get_ip_address());
-    wifi->get_netmask(&a);
-    pc.printf("Netmask: %s\n", a.get_ip_address());
-    wifi->get_gateway(&a);
-    pc.printf("Gateway: %s\n", a.get_ip_address());
-    pc.printf("RSSI: %d\n\n", wifi->get_rssi());
-
- 
-	const char body[] = "{\"hello\":\"world\"}";
- 
-	HttpRequest* request = new HttpRequest(wifi, HTTP_POST, "http:www.example.com");
-	request->set_header("Content-Type", "application/json");
-	HttpResponse* response = request->send(body, strlen(body));
-	// if response is NULL, check response->get_error()
- 
-	printf("status is %d - %s\n", response->get_status_code(), response->get_status_message());
-	printf("body is:\n%s\n", response->get_body_as_string().c_str());
- 
-	delete request; // also clears out the response
-
-*/
 
 	http();
-	
-    wifi->disconnect();
-    pc.printf("\nDone\n");    
+  pc.printf("\nDone\n");    
 }
 
 
 int main() {
-    wifi = WiFiInterface::get_default_instance();
-    if (!wifi) {
-        printf("ERROR: No WiFiInterface found.\n");
-        return -1;
-    }
-
     t.start(callback(&queue, &EventQueue::dispatch_forever));
     button.fall(queue.event(pressed_handler));
     pc.printf("Starting\n");
