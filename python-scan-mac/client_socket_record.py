6bbfc2ba78fcf78eae42670b2b15d90991c8fa42
Surachai Rodsai <surachairobotic@gmail.com>
2020-03-13 13:19:20 +0700

    server and client socket

--- python-scan-mac/client_socket_record.py
+++ python-scan-mac/client_socket_record.py
@@ -1,12 +1,55 @@
 import socket
 
+import asyncio
+from time import sleep
+from bleak import discover
+
+from datetime import datetime
+
+import sys
+
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-NAME = 'toa_iot'
+PORT = 3333        # Port to listen on (non-privileged ports are > 1023)
+NAME = 'xxx'
+mac_addrs = ("D6:07:04:2B:F7:B1") # surachai
+b_scan = False
+
+
+class ClientTCP:
+  def __init__(self):
+    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+  def connect_server(self, _host='127.0.0.1', _port=5555):
+    self.sock.connect((_host, _port))
+    
+  def terminate(self):
+    self.sock.close()
+
+
+client = ClientTCP()
 
 def main():
-  client = ClientTCP()
+  global client, mac_addrs, NAME
+
+  l_name = ['toa_iot', 'bank_iot', 'inn_iot']
+  NAME = l_name[int(str(sys.argv[1]))]
+  print(NAME)
+
   client.connect_server(HOST, PORT)
+
+  loop = asyncio.get_event_loop()
+  loop.create_task(scan())
+  try:
+      loop.run_forever()
+  except SomeException as e:
+      loop.close()
+      print('loop.close()')
+      client.terminate()
+      print('terminate')
+  print('END')
+
+async def scan():
+  global mac_addrs, b_scan, client, NAME
   while True:    
     txt = client.sock.recv(64)
     txt = txt.decode('utf-8')
@@ -17,13 +60,39 @@ def main():
       print("true")
       msg = NAME
       client.sock.send(bytes(msg,"utf-8"))
+    elif txt == 'save':
+      b_scan = True
+    elif txt == 'q':
+      print('receive --> q')
+      raise Exception('my exit')
 
-class ClientTCP:
-  def __init__(self):
-    self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-  def connect_server(self, _host='127.0.0.1', _port=5555):
-    self.sock.connect((_host, _port))
+    if b_scan:
+        print('Start scanning')
+        tstart = datetime.now()
+        devices = await discover()
+        print('Found %d devices'%(len(devices)))
+        for i in range(len(devices)):
+            print("[%d] : %s" % (i, devices[i]))
+        b_found = False
+        rssi = 0
+        for dev in devices:
+            dev_mac = str(dev).split(': ')[0]
+            if dev_mac in mac_addrs:
+                #print(dev_mac, 'detected at', dev.rssi, 'dBm')
+                print(dev.rssi)
+                rssi = dev.rssi
+                b_found = True
+        telapsed = datetime.now() - tstart
+        print('Elapsed time: %s'%(telapsed))
+        #await asyncio.sleep(6 - telapsed)
+        b_scan = False
+        if b_found:
+          msg = str('%s:%d' % (NAME, rssi))
+          client.sock.send(bytes(msg,"utf-8"))
+        else:
+          msg = str('%s:NOT' % (NAME))
+          client.sock.send(bytes(msg,"utf-8"))
+
     
 if __name__ == '__main__':
   main()
--- python-scan-mac/server_socket_record.py
+++ python-scan-mac/server_socket_record.py
@@ -1,23 +1,36 @@
 import socket
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+PORT = 3333        # Port to listen on (non-privileged ports are > 1023)
 
 def main():
   server = ServerTCP(HOST, PORT)
-  server.start_server()
+  people = server.start_server()
+  
+  f = open('/home/cobot/taist/software_embedded_systems/stm32l475-iot-node/python-scan-mac/sync_data.txt', 'w+')
+  f.close()
+  
+  key = 'x'
+  while True:
+    key = input('Enter your input: ')
+    if key == 's':
+      for i in range(len(people)):
+        people[i].conn.sendall(bytes('save',"utf-8"))
+    elif key == 'q':
+      break
+    incoming_txt = ["", "", ""]
+    for i in range(len(people)):
+      incoming_txt[i] = people[i].conn.recv(1024).decode('utf-8')
+    print(incoming_txt)
+    f = open('/home/cobot/taist/software_embedded_systems/stm32l475-iot-node/python-scan-mac/sync_data.txt', 'a')
+    f.writelines(str("\n%s,%s,%s" % (incoming_txt[0], incoming_txt[1], incoming_txt[2])))
+    f.close()
+
+  for i in range(len(people)):
+    print('send q : ', i)
+    people[i].conn.sendall(bytes('q',"utf-8"))
+
   exit()
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-      s.bind((HOST, PORT))
-      s.listen()
-      conn, addr = s.accept()
-      with conn:
-          print('Connected by', addr)
-          while True:
-              data = conn.recv(1024)
-              if not data:
-                  break
-              conn.sendall(data)
 
 class ServerTCP:
   def __init__(self, _host='127.0.0.1', _port=5555):
@@ -29,7 +42,7 @@ class ServerTCP:
     self.sock.bind((self.host, self.port))
     self.sock.listen(5)
     id_list = ['toa_iot','bank_iot','inn_iot']
-    data = [rssiData('toa_iot'), rssiData('bank_iot'), rssiData('inn_iot')]
+    people = [rssiPeople('toa_iot'), rssiPeople('bank_iot'), rssiPeople('inn_iot')]
     i=0
     while i!=3:
       print("waiting for a client")
@@ -37,17 +50,22 @@ class ServerTCP:
       print("accept")
       conn.sendall(bytes('#id$',"utf-8"))
       print("ask id")
-      txt = conn.recv(64)
+      txt = conn.recv(1024)
       txt = txt.decode('utf-8')
-      for k in range(len(data)):
-        if data[k].name == txt:
-          print("%s : (%s, %s)" % (data[k].name, conn, addr))
-          data[k].pair = (conn, addr)
+      print(txt)
+      for k in range(len(people)):
+        if people[k].name == txt:
+          print("%s : (%s, %s)" % (people[k].name, conn, addr))
+          print(type(conn))
+          print(type(addr))
+          people[k].conn = conn
+          i+=1
+    return people
       
-class rssiData:
+class rssiPeople:
   def __init__(self, _name):
     self.name = _name
-    self.pair = (0, 0)
+    self.conn = socket.socket()
     
 if __name__ == '__main__':
   main()
